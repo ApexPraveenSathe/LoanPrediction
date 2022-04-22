e303b986b8baf29aa712bf8d1ec7d83dc4082d67
ApexPraveenSathe <93828388+ApexPraveenSathe@users.noreply.github.com>
2022-04-22 23:58:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,8 +2,7 @@ import pickle
 import streamlit as st
  
 # loading the trained model
-pickle_in = open('classifier.pkl', 'rb') 
-classifier = pickle.load(pickle_in)
+classifier = pickle.load(open('classifier.pkl', 'rb'))
  
 @st.cache()
   
